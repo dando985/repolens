79792fac35b84bedae49dd291ae7b748dd9a1aaf
Dan Do <dando985@gmail.com>
2026-08-29 10:23:45 -0500

    added embedding provider and exception handling class

---
+++ src/main/java/EmbeddingException.java
@@ -0,0 +1,10 @@
+public class EmbeddingException extends RuntimeException {
+
+    public EmbeddingException(String message) {
+        super(message);
+    }
+
+    public EmbeddingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/EmbeddingProvider.java
@@ -0,0 +1,4 @@
+public interface EmbeddingProvider {
+
+    double[] createEmbedding(String text);
+}
