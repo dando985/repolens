0c2cf41912ce811c271f16ed169586bae3390095
Dan Do <dando985@gmail.com>
2026-09-01 14:03:23 -0500

    added function comments

--- src/main/java/EmbeddingProvider.java
+++ src/main/java/EmbeddingProvider.java
@@ -1,5 +1,7 @@
 public interface EmbeddingProvider {
 
+    // Needed to avoid mixing embeddings from different models in the same index file.
+    // The model name is stored in the index file and checked when loading the index.
     String getModelName();
 
     double[] createEmbedding(String text);
