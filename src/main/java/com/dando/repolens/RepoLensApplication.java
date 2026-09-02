479abe9e7db80cac4ce7c0353c9aaa4ed752ad9e
Dan Do <dando985@gmail.com>
2026-09-02 11:10:51 -0500

    added Spring entry point

---
+++ src/main/java/com/dando/repolens/RepoLensApplication.java
@@ -0,0 +1,12 @@
+package com.dando.repolens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoLensApplication.class, args);
+    }
+}
