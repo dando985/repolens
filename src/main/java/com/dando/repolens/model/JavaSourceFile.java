189421a7d07e3421816e6a471b7a05ba296170db
Dan Do <dando985@gmail.com>
2026-09-09 14:09:15 -0500

    added class comment

--- src/main/java/com/dando/repolens/model/JavaSourceFile.java
+++ src/main/java/com/dando/repolens/model/JavaSourceFile.java
@@ -2,6 +2,7 @@ package com.dando.repolens.model;
 
 import java.nio.file.Path;
 
+// Maps a Java source file to its path and source code.
 public class JavaSourceFile {
 
     private final Path path;
